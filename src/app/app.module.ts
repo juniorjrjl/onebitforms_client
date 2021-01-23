2ed83a6956a4fbceedeae1bf546879384e04510f
Junior <junior.jr.jl@gmail.com>
2021-01-23 20:14:26 -0300

    criaçao da pagina home e not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ChartsModule } from 'ng2-charts';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule } from './forms/forms.module';
+import { AnswersModule } from './answers/answers.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    ColorPickerModule,
+    ChartsModule,
+    FormsModule,
+    AnswersModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
